fa3d88e8bcdc87252c9b77ae4347563996a7e09f
mcastanon-2019078 <mcastanon-2019078@kinal.edu.gt>
2024-05-30 19:17:30 -0600

    Estructura BackEnd Realizada

---
+++ SystemBank-NodeJS/configs/app.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import { config } from 'dotenv'
+ 
+const app = express() //creamos el servidor
+config()
+const port = process.env.PORT || 3200
+ 
+//Configuramos el servidor express
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+ 
+//Aceptamos o denegamos las solicitudes de diferentes origenes(local, remoto)
+app.use(cors())
+app.use(helmet())
+ 
+//Crear logs de solicitudes al servidor HTTP
+app.use(morgan('dev'))
+ 
+export const initServer = () =>{
+    app.listen(port)
+    console.log(`Server HTTP running in port ${port}`)
+}
