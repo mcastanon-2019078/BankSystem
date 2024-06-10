50d5fad1d30608a995fdb8b23486976bb5bb9e24
wcabrera-2022060 <wcabrera-2022060@kinal.edu.gt>
2024-06-09 21:42:03 -0600

    Se agregaron las funcionalidades de productos, se modificó el back para poder añadirles seguridad por medio de los roles, también ahora es posible observar los tipos de cuenta

--- bank-system-api/src/product/product.routes.js
+++ bank-system-api/src/product/product.routes.js
@@ -1,14 +1,15 @@
 'use strict'
 
 import { Router } from "express"
+import { validateRoleAdmin, validateToken } from '../../middlewares/validateToken.js'
 import { addProduct, deleteProduct, getByIdProduct, getProduct, updateP } from './product.controller.js'
 
 const api = Router()
 
-api.post('/add', addProduct);
-api.put('/update/:id', updateP);
-api.delete('/delete/:id', deleteProduct);
-api.get('/get', getProduct);
-api.get('/getById/:id', getByIdProduct);
+api.post('/add', [validateToken, validateRoleAdmin], addProduct);
+api.put('/update/:id', [validateToken, validateRoleAdmin], updateP);
+api.delete('/delete/:id', [validateToken, validateRoleAdmin], deleteProduct);
+api.get('/get', [validateToken, validateRoleAdmin], getProduct);
+api.get('/getById/:id', [validateToken, validateRoleAdmin], getByIdProduct);
 
 export default api
--- bank-system-api/src/typeAccount/typeAccount.routes.js
+++ bank-system-api/src/typeAccount/typeAccount.routes.js
@@ -1,14 +1,15 @@
 'use strict'
 
 import { Router } from "express"
-import { addTypeA, getByIdTypeA, getTypeA, deleteTypeA, updateTypeA } from './typeAccount.controller.js'
+import { validateRoleAdmin, validateToken } from '../../middlewares/validateToken.js'
+import { addTypeA, deleteTypeA, getByIdTypeA, getTypeA, updateTypeA } from './typeAccount.controller.js'
 
 const api = Router()
 
-api.post('/add', addTypeA);
-api.put('/update/:id', updateTypeA);
-api.delete('/delete/:id', deleteTypeA);
-api.get('/get', getTypeA);
-api.get('/getById/:id', getByIdTypeA);
+api.post('/add', [validateToken, validateRoleAdmin], addTypeA);
+api.put('/update/:id', [validateToken, validateRoleAdmin], updateTypeA);
+api.delete('/delete/:id', [validateToken, validateRoleAdmin], deleteTypeA);
+api.get('/get', [validateToken, validateRoleAdmin], getTypeA);
+api.get('/getById/:id', [validateToken, validateRoleAdmin], getByIdTypeA);
 
 export default api
