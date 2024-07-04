9313adda55fc2d134ecd34c5005770dc58badcce
wcabrera-2022060 <wcabrera-2022060@kinal.edu.gt>
2024-07-03 20:20:49 -0600

    Creación de cuentas realizado

--- bank-system-api/src/account/account.routes.js
+++ bank-system-api/src/account/account.routes.js
@@ -1,17 +1,18 @@
 'use strict'
 
-import { Router } from "express"
+import { Router } from "express";
+import { validateRoleAdmin, validateToken } from '../../middlewares/validateToken.js';
 import { addAccount, deleteAccount, getAccounts, getByIdAccount, getByUser, movementsHight, movementsUnder, updateAccount } from "./account.controller.js";
 
 const api = Router();
 
-api.post('/add', addAccount);
-api.put('/update/:id', updateAccount);
-api.delete('/delete/:id', deleteAccount);
-api.get('/get', getAccounts);
-api.get('/getById/:id', getByIdAccount);
-api.get('/getMovementsHigh', movementsHight);
-api.get('/getMovementsUnder', movementsUnder);
-api.get('/getByUser/:id', getByUser);
+api.post('/add', [validateToken, validateRoleAdmin], addAccount);
+api.put('/update/:id', [validateToken, validateRoleAdmin], updateAccount);
+api.delete('/delete/:id', [validateToken, validateRoleAdmin], deleteAccount);
+api.get('/get', [validateToken, validateRoleAdmin], getAccounts);
+api.get('/getById/:id', [validateToken, validateRoleAdmin], getByIdAccount);
+api.get('/getMovementsHigh', [validateToken, validateRoleAdmin], movementsHight);
+api.get('/getMovementsUnder', [validateToken, validateRoleAdmin], movementsUnder);
+api.get('/getByUser/:id', [validateToken, validateRoleAdmin], getByUser);
 
 export default api;
